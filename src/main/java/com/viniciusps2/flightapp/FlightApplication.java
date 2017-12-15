d5ee00830a3a82dadb43f9e546348a7d890a82d4
Vinicius Sanches <vinicius.sjc@gmail.com>
2017-12-15 21:09:05 -0200

    creating initial structure

---
+++ src/main/java/com/viniciusps2/flightapp/FlightApplication.java
@@ -0,0 +1,16 @@
+package com.viniciusps2.flightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
+public class FlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightApplication.class, args);
+	}
+}
---
+++ src/test/java/com/viniciusps2/flightapp/FlightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.viniciusps2.flightapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
