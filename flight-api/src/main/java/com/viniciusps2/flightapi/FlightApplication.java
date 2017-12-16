b4cf5c26cd3f9eece365bd54a0bd0a0692f3faa0
Vinicius Sanches <vinicius.sjc@gmail.com>
2017-12-16 18:01:49 -0200

    enable cors

--- flight-api/src/main/java/com/viniciusps2/flightapi/FlightApplication.java
+++ flight-api/src/main/java/com/viniciusps2/flightapi/FlightApplication.java
@@ -3,7 +3,11 @@ package com.viniciusps2.flightapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan
@@ -13,4 +17,16 @@ public class FlightApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+				// .allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 }
