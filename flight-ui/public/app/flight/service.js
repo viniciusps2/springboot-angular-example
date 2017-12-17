90bbd23c1edf0ce909ab8b9991cecb2ed4d40e3e
Vinicius Sanches <vinicius.sjc@gmail.com>
2017-12-17 16:09:44 -0200

    refactoring modal service

---
+++ flight-ui/public/app/flight/service.js
@@ -0,0 +1,33 @@
+;(function () {
+  'use strict'
+
+  angular.module('flight-ui').service('FlightService', FlightService)
+
+  FlightService.$inject = ['$uibModal', 'Flight', 'ModalService']
+
+  function FlightService ($uibModal, Flight, ModalService) {
+    return {
+      create: create,
+      view: view
+    }
+
+    function view (flightId) {
+      return Flight.findById(flightId).then(function (flight) {
+        return ModalService.open({
+          templateUrl: 'app/flight/modal.html',
+          title: 'Flight nº ' + flight.id,
+          flight: flight
+        })
+      })
+    }
+
+    function create () {
+      return ModalService.open({
+        templateUrl: 'app/flight/modal.html',
+        title: 'New Flight',
+        flight: {},
+        onSave: Flight.create
+      })
+    }
+  }
+})()
